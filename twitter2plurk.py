1204592d18c0ba32bac524643442f5121ed44d3f
Gea-Suan Lin <gslin@gslin.org>
2018-02-15 20:33:14 +0800

    Add class skeleton.

--- twitter2plurk.py
+++ twitter2plurk.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
+class Twitter2Plurk(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
 if '__main__' == __name__:
-    pass
+    t = Twitter2Plurk()
+    t.start()
