c11cae19a9ee6400420e165445c5f8f54fe99939
Gea-Suan Lin <gslin@gslin.org>
2018-02-15 20:36:31 +0800

    Generate config filename.

--- twitter2plurk.py
+++ twitter2plurk.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import os
+
 class Twitter2Plurk(object):
     def __init__(self):
         pass
 
     def start(self):
-        pass
+        f = '{}/.config/twitter2plurk/config.ini'.format(os.environ['HOME'])
 
 if '__main__' == __name__:
     t = Twitter2Plurk()
