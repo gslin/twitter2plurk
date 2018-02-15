e6b8ad899b3b41da85095f1a9ea32829a7cc3106
Gea-Suan Lin <gslin@gslin.org>
2018-02-15 20:39:31 +0800

    Read twitter key/secret.

--- twitter2plurk.py
+++ twitter2plurk.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import configparser
 import os
 
 class Twitter2Plurk(object):
@@ -8,6 +9,14 @@ class Twitter2Plurk(object):
 
     def start(self):
         f = '{}/.config/twitter2plurk/config.ini'.format(os.environ['HOME'])
+
+        c = configparser.ConfigParser()
+        c.read(f)
+
+        self.twitter_ac = c['default']['twitter_access_token'])
+        self.twitter_as = c['default']['twitter_access_token_secret'])
+        self.twitter_ck = c['default']['twitter_consumer_key'])
+        self.twitter_cs = c['default']['twitter_consumer_secret'])
 
 if '__main__' == __name__:
     t = Twitter2Plurk()
