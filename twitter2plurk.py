1c4bb4570ddba1dbe035fdc7493f1c90785a6e34
Gea-Suan Lin <gslin@gslin.org>
2023-05-25 08:10:41 +0800

    Migrate to json feed source.

--- twitter2plurk.py
+++ twitter2plurk.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import configparser
-import html
 import plurk_oauth
 import os
+import requests
 import sqlite3
 import time
-import twitter
-import urllib
 
 class Twitter2Plurk(object):
     def __init__(self):
@@ -21,12 +19,8 @@ class Twitter2Plurk(object):
         c = configparser.ConfigParser()
         c.read(f_conf)
 
-        t_ak = c['default']['twitter_access_token']
-        t_as = c['default']['twitter_access_token_secret']
-        t_ck = c['default']['twitter_consumer_key']
-        t_cs = c['default']['twitter_consumer_secret']
-        t_user = c['default']['twitter_username']
-        t = twitter.Api(access_token_key=t_ak, access_token_secret=t_as, consumer_key=t_ck, consumer_secret=t_cs)
+        jsonfeed_url = c['default']['twitter_rssbridge_jsonfeed_url']
+        items = requests.get(jsonfeed_url).json()['items']
 
         p_ak = c['default']['plurk_app_key']
         p_as = c['default']['plurk_app_secret']
@@ -40,22 +34,21 @@ class Twitter2Plurk(object):
         sql_insert = 'INSERT INTO entry (twitter_id, created_at) VALUES (?, ?);'
         sql_select = 'SELECT COUNT(*) FROM entry WHERE twitter_id = ?;'
 
-        for status in sorted(list(t.GetUserTimeline(screen_name=t_user)), key=lambda x: x.id):
+        for item in items:
             # Skip if it's retweet.
-            if status.retweeted:
+            if item['title'].startswith('@'):
                 continue
 
             # Generate "text" with unescape (workaround).
-            text = html.unescape(status.text)
-            for u in status.urls:
-                text = text.replace(u.url, u.expanded_url)
+            text = item.title
 
-            # Generate "url"
-            url = 'https://twitter.com/{}/status/{}'.format(urllib.parse.quote(t_user), urllib.parse.quote(status.id_str))
+            # Generate parameters.
+            id_str = item['url'].split('/')[-1]
+            url = item['url']
 
             c = s.cursor()
 
-            c.execute(sql_select, (status.id_str, ))
+            c.execute(sql_select, (id_str, ))
             if 0 == c.fetchone()[0]:
                 content = '{}\n\n{}'.format(text, url)
                 print('* content = {}'.format(content))
@@ -65,12 +58,12 @@ class Twitter2Plurk(object):
                     'qualifier': ':',
                 })
 
-                print('* type(status) = {}'.format(type(status)))
-                print('* status = {}'.format(status))
+                print('* type(item) = {}'.format(type(item)))
+                print('* item = {}'.format(item))
                 print('* type(res) = {}'.format(type(res)))
                 print('* res = {}'.format(res))
                 if type(res) is dict and res['plurk_id'] > 0:
-                    c.execute(sql_insert, (status.id_str, int(time.time())))
+                    c.execute(sql_insert, (id_str, int(time.time())))
                     s.commit()
                 else:
                     s.rollback()
